82a79f8acde8c3baa89978f57a234f33b7a5b910
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-08-22 13:12:48 +0300

    Added search bar for gifs

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const img = document.querySelector('img');
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=eij09x8kAsgVcnPBsuZ4Z7V0FgQunCSf&s=cats', { mode: 'cors' })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-        img.src = response.data.images.original.url;
-    });
+const search = document.getElementById('search-input');
+const form = document.getElementById('search-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let searchInput = search.value;
+    loadGif(searchInput);
+})
+
+function loadGif(search) {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=eij09x8kAsgVcnPBsuZ4Z7V0FgQunCSf&s=${search}`, { mode: 'cors' })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            if (response.data) {
+                img.src = response.data.images.original.url;
+            } else {
+                loadGif('cats');
+            }
+        })
+        .catch(function () {
+            loadGif('cats');
+        });
+}
+
+loadGif();
